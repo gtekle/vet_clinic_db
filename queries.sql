8a5eabae872997cda706fc093322055b4f2c5d4e
tYohannes <teklegy@gmail.com>
2022-03-21 17:33:48 +0300

    Fix animals table query by date_of_birth range according to code review feedback

--- queries.sql
+++ queries.sql
@@ -1,15 +1,15 @@
 /*Queries that provide answers to the questions from all projects.*/
 
--- Find all animals whose name ends in "mon".
+-- Find all animals whose name ends in "mon"
 SELECT * FROM animals WHERE name like '%mon';
 
--- SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
-SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+-- List the name of all animals born between 2016 and 2019
+SELECT name FROM animals WHERE date_of_birth >= '2016-01-01' AND date_of_birth <= '2019-12-31';
 
--- List the name of all animals that are neutered and have less than 3 escape attempts.
+-- List the name of all animals that are neutered and have less than 3 escape attempts
 SELECT name from animals WHERE neutered AND escape_attempts < 3;
 
--- List date of birth of all animals named either "Agumon" or "Pikachu".
+-- List date of birth of all animals named either "Agumon" or "Pikachu"
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 
 -- List name and escape attempts of animals that weigh more than 10.5kg
